59e7cd2f2b9c5f0a486fabe1648b51c91a9c48e6
jjworapol <worapol.n@ku.th>
2019-02-10 04:58:09 +0700

    Add files via upload

--- jquery3/myscript.js
+++ jquery3/myscript.js
@@ -21,9 +21,7 @@ $(document).ready(function() {
     // convert selects already on the page at dom load
 //   $('select.form-control').combobox();	
     
-  $('#it').click(function(e){
-    $('ul.dropdown-menu').toggle();
-  }); 
+  
 
   
 });
